f97baf2ae3c63bd9468fdbc10c5cca71f1747f37
sfasfasfas12 <gs1geielred@hotmail.com>
2024-05-07 15:24:41 +0700

    feat commit

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-function findMaxConsecutiveOnes(nums) {
-  let maxCount = 0;
-  let count = 0;
-  for (const num of nums) {
-    if (num === 1) {
-      count++;
-      maxCount = Math.max(maxCount, count);
-    } else count = 0;
+function threeSum(nums) {
+  nums.sort((a, b) => a - b);
+  const result = [];
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (i === 0 || (i > 0 && nums[i] !== nums[i - 1])) {
+      let low = i + 1;
+      let high = nums.length - 1;
+      const sum = 0 - nums[i];
+      while (low < high) {
+        if (nums[low] + nums[high] === sum) {
+          result.push([nums[i], nums[low], nums[high]]);
+          while (low < high && nums[low] === nums[low + 1]) low++;
+          while (low < high && nums[high] === nums[high - 1]) high--;
+          low++;
+          high--;
+        } else if (nums[low] + nums[high] < sum) low++;
+        else high--;
+      }
+    }
   }
-  return maxCount;
+  return result;
 }
