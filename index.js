bc4f32e4e14d6378fe99b58ea7d00d2c8f153a66
sfasfasfas12 <gs1geielred@hotmail.com>
2024-05-09 05:28:35 +0700

    hello commit

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
-function threeSum(nums) {
-  nums.sort((a, b) => a - b);
+function combinationSum2(candidates, target) {
+  candidates.sort((a, b) => a - b);
   const result = [];
-  for (let i = 0; i < nums.length - 2; i++) {
-    if (i === 0 || (i > 0 && nums[i] !== nums[i - 1])) {
-      let low = i + 1;
-      let high = nums.length - 1;
-      const sum = 0 - nums[i];
-      while (low < high) {
-        if (nums[low] + nums[high] === sum) {
-          result.push([nums[i], nums[low], nums[high]]);
-          while (low < high && nums[low] === nums[low + 1]) low++;
-          while (low < high && nums[high] === nums[high - 1]) high--;
-          low++;
-          high--;
-        } else if (nums[low] + nums[high] < sum) low++;
-        else high--;
-      }
+  backtrack([], 0, 0);
+  return result;
+  function backtrack(combination, start, sum) {
+    if (sum === target) {
+      result.push([...combination]);
+      return;
+    }
+    if (sum > target) return;
+    for (let i = start; i < candidates.length; i++) {
+      if (i > start && candidates[i] === candidates[i - 1]) continue;
+      combination.push(candidates[i]);
+      backtrack(combination, i + 1, sum + candidates[i]);
+      combination.pop();
     }
   }
-  return result;
 }
